a8a377a320720b9a8271a8800bf2738f87eb415b
Nathan Feaver <nathan.feaver@gmail.com>
2016-04-08 13:07:59 -0600

    add a global gitignore

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ ln -s ~/dotfiles/rvmrc ~/.rvmrc
 ln -s ~/dotfiles/gemrc ~/.gemrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+# Run this command to add the global gitignore
+# git config --global core.excludesfile ~/.gitignore_global
+ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
