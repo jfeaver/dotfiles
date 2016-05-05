5cecf8de9b688e0f0e8d52d27f4b7d9348d37845
Nathan Feaver <nathan.feaver@gmail.com>
2016-05-05 13:59:21 -0600

    add gitconfig to dotfiles

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ ln -s ~/dotfiles/rvmrc ~/.rvmrc
 ln -s ~/dotfiles/gemrc ~/.gemrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-# Run this command to add the global gitignore
-# git config --global core.excludesfile ~/.gitignore_global
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
 # Run this command to make start executable:
 # chmod +x /usr/local/bin/start
